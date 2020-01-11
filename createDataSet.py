832e46e40aef963cfcc577f41205ae6300f72e90
saikkonen <veikko.saikkonen@gmail.com>
2020-01-11 23:36:54 +0200

    Update createDataSet.py

--- createDataSet.py
+++ createDataSet.py
@@ -9,11 +9,26 @@ import pandas as pd
 import numpy as np
 import os
 
-
+# Gather all data
 allStockData = pd.read_csv('stockData_clean.csv')
 allTweets = pd.read_csv('x_all.csv')
 
+headers = ['Month', 'Day', 'Year']
+
+for i in range(allTweets.shape[1] - 3):
+    headers.append(i)
+allTweets = pd.read_csv('x_all.csv', names = headers)
+
+print(allTweets.head(5))
 
 
 
+#tweetsWithStocks = allTweets.merge(allStockData, left_on = )
+
+#print(allTweets['Month'].head(5))
+
+#print(tweetsWithStocks.head(5))
+print(allTweets.shape)
+print(allStockData.shape)
+#print(tweetsWithStocks.shape)
 
