5bfd188f8ea064aed525698780185ac22b3600ae
saikkonen <veikko.saikkonen@gmail.com>
2020-01-11 22:18:25 +0200

    Created buildDataSet.py

---
+++ buildDataSet.py
@@ -0,0 +1,13 @@
+#{
+#   With Twitter data and Stock data set up, build a dataset that matches Twitter data with corresponsind stock value changes
+#   during the next day. (Timeframe can be changed later.)
+# 
+# }
+
+
+import pandas as pd
+import numpy as np
+import os
+
+
+
