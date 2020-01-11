46364ef82f76ec3bbb42e445fe5fa4b32b9dc83f
saikkonen <veikko.saikkonen@gmail.com>
2020-01-11 21:24:29 +0200

    Stock data and associated scripts created

--- stockData.py
+++ stockData.py
@@ -6,22 +6,102 @@
 #                                                       'closing value 0'     'closing value 0' ...
 # }                                                         ...                     ...
 import pandas as pd
+import numpy as np
+import os
+
+# Function declarations
+
+def verifyFolder(folder):
+    fileNames = os.listdir(folder)
+    nofFiles = len(fileNames)
+    nofRows = np.zeros(nofFiles)
+
+    for index in range(nofFiles):
+        contents = pd.read_csv(folder + fileNames[index])
+        contents_date = contents['Date']
+        nofRows[index] = len(contents_date)
+        print(fileNames[index])
+
+    temp = nofRows[0]
+
+    for lenIndex in range(nofFiles):
+        currentLength = nofRows[lenIndex]
+        if(temp != currentLength):
+            print("ERROR, length was : " + str(currentLength) + ", temp was : " + str(temp))
+            print("Filename was : " + fileNames[lenIndex])
+            return -1
+        else:
+            print("OK")
+    return 1
 
 
 def appendData(nameOfDataFile, appendTo):
     
     input = pd.read_csv(nameOfDataFile)
+    oldData = pd.read_csv(appendTo)
+    #Find the index where the filename starts
+    csvIndex = 0
+    for charIndex in range(len(nameOfDataFile)):
+        char = nameOfDataFile[charIndex]
+        if(char == '/'):
+            lastSlashIndex = charIndex
+        if(char == '.' and charIndex > csvIndex):
+            csvIndex = charIndex
 
+    sliceObj = slice(lastSlashIndex + 1, csvIndex)
 
-    #writeTo = pd.to_csv(appendTo, mode='a', header=False)
+    stockName = nameOfDataFile[sliceObj]
+    newClosingValues = pd.DataFrame(data = input['Close'])
+    newClosingValues.columns = [stockName]
+    
+    allClosingValues = pd.DataFrame(data = oldData) 
 
+    if stockName in allClosingValues:
+        allClosingValues[stockName] = newClosingValues[stockName]
+    else:
+        allClosingValues = allClosingValues.join(newClosingValues)
+    
+    allClosingValues.to_csv(appendTo, header = True, mode = 'w', index = False)
 
     return
 
+def setDates(nameOfDataFile, appendTo):
+    
+    input = pd.read_csv(nameOfDataFile)
+
+    dates = pd.DataFrame(data = input['Date'])
+    
+    dates.to_csv(appendTo, header = True, mode = 'w', index = False)
+
+    return
+
+
+#{
+#   Script begins here
+# }
 outputFile = 'stockData_clean.csv'
-folder = './StocksnShit'
-testFile = 'LMT.csv'
+folder = './StocksnShit/'
+testFile = 'AAL.csv'
 
-fullPath = folder + "/" + testFile
 
-appendData(fullPath, outputFile)
+if(verifyFolder(folder) == -1):
+    print("Error in verifying folder.")
+    exit
+else:
+    print("Folder row lengths ok")
+
+
+fileNames = os.listdir(folder)
+nofFiles = len(fileNames)
+nofRows = np.zeros(nofFiles)
+
+setDates(folder + fileNames[0], outputFile)
+    
+
+for fileIndex in range(nofFiles):
+    currentFile = fileNames[fileIndex]
+    fullPath = folder + currentFile
+    appendData(fullPath, outputFile)
+
+
+
