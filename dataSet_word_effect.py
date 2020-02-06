8e19deaf6294a4fae44c0fbf20c0796d621dd55f
johmakinen <joh.makinen@gmail.com>
2020-02-06 18:00:32 +0200

    added avg for words

--- dataSet_word_effect.py
+++ dataSet_word_effect.py
@@ -11,20 +11,28 @@ colnames = df_all.iloc[1:2, range(3, 17)].columns.values
 number_of_words = df_all.shape[1]-3-len(colnames)
 # print(number_of_words)
 
-effects_per_word = pd.DataFrame(columns=colnames)
+effects_per_word_mad = pd.DataFrame(columns=colnames)
+effects_per_word_avg = pd.DataFrame(columns=colnames)
 # print(effects_per_word)
 i = 0
 
 while i < number_of_words:
     temp = df_all.loc[df_all[str(i)] > 0]
-    means = temp[colnames].mad(axis=0)
+    mads = temp[colnames].mad(axis=0)
+    avgs = temp[colnames].mean(axis=0)
     #effects_per_word.iloc[i, :] = means.values
-    effects_per_word.loc[len(effects_per_word)] = np.around(
-        means.values, 5)  # adding a row
+    effects_per_word_mad.loc[len(effects_per_word_mad)] = np.around(
+        mads.values, 5)  # adding a row
+    effects_per_word_avg.loc[len(effects_per_word_avg)] = np.around(
+        avgs.values, 5)
     i += 1
 
-effects_per_word = effects_per_word.fillna(0)
+effects_per_word_mad = effects_per_word_mad.fillna(0)
+effects_per_word_avg = effects_per_word_avg.fillna(0)
 # print(effects_per_word)
-effects_per_word["Words"] = key_index
-effects_per_word.to_csv('effects_per_word.csv',
-                        header=True, index=True, sep=';', decimal=',', na_rep='blank')
+effects_per_word_mad["Words"] = key_index
+effects_per_word_avg["Words"] = key_index
+effects_per_word_mad.to_csv('effects_per_word_mad.csv',
+                            header=True, index=True, sep=';', decimal=',', na_rep='blank')
+effects_per_word_avg.to_csv('effects_per_word_avg.csv',
+                            header=True, index=True, sep=';', decimal=',', na_rep='blank')
